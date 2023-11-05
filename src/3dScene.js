de6be8938d14d0eaee2271524abcfd2b354874e9
rik51 <animeboy3228@outlook.com>
2023-11-06 02:13:02 +0700

    Day end

--- src/3dScene.js
+++ src/3dScene.js
@@ -1,11 +1,8 @@
 import {CubeCamera, Environment, OrbitControls, PerspectiveCamera} from "@react-three/drei";
-import {Boxes} from "./Box";
-import {Ground} from "./Ground";
 import {Bloom, ChromaticAberration, EffectComposer} from "@react-three/postprocessing";
 import {BlendFunction} from "postprocessing";
 import React, {useRef} from "react";
-import {Masks} from "./Masks";
-
+import {Me} from "./Mi3d";
 export function  Scene(){
     let ref = useRef(PerspectiveCamera)
     console.log(ref)
@@ -15,24 +12,15 @@ export function  Scene(){
             <OrbitControls target={[0,2,0]} maxPolarAngle={1.45}/>
             <PerspectiveCamera makeDefault fov={50} position={[8,16,-30]} ref={ref}/>
             <color args={[0,0,0]} attach="background"/>
-            {/*<Boxes/>*/}
             <CubeCamera resolution={256} frames={Infinity}>
                 {(texture)=>(
                     <>
                         <Environment map={texture}/>
-                        <Masks/>
+                        <Me/>
                     </>
                 )}
             </CubeCamera>
-            {/*<spotLight*/}
-            {/*    color={[0.255, 0.215, 0]}*/}
-            {/*    intensity={1.5}*/}
-            {/*    angle={90}*/}
-            {/*    penumbra={0.5}*/}
-            {/*    position={[1, 1, 1]}*/}
-            {/*    castShadow*/}
-            {/*    shadow-bias={-0.0001}*/}
-            {/*/>*/}
+
             <spotLight
                 color={[0.255, 0.255, 0.255]}
                 intensity={2}
@@ -44,7 +32,6 @@ export function  Scene(){
             />
 
             <EffectComposer>
-                {/*<DepthOfField focusDistance={0.0025} focalLength={0.01} bokehScale={3} height={480}/>*/}
 
                 <Bloom
                     blendFunction={BlendFunction.ADD}
@@ -55,7 +42,6 @@ export function  Scene(){
                     luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
                     luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
                 />
-                {/*<FGrid/>*/}
                 <ChromaticAberration
                     blendFunction={BlendFunction.NORMAL} // blend mode
                     offset={[0.0005, 0.0012]} // color offset
